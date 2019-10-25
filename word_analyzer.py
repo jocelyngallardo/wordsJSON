39677762460f661a5db36924b0cb7ad0d516a2de
jocelyngallardo <54326467+jocelyngallardo@users.noreply.github.com>
2019-10-25 09:38:07 -0700

    add code

--- word_analyzer.py
+++ word_analyzer.py
@@ -3,4 +3,14 @@ import json
 with open('words.json') as words:
     word_data = json.load(words)
     
-print(word_data[0])
+#print(word_data[0]['word'])
+for data in word_data:
+    if data['part of speech'] == 'verb':
+        print(data['word'])
+        
+num_noun = 0
+
+for data in word_data:
+    if data['part of speech'] == 'noun':
+        num_noun+=1
+print(num_noun)
